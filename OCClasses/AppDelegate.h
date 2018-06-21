f05bc36d6111912ce14c39ba8c8b6f757d5e0a3e
qingfeng <qingfengiosdev@gmail.com>
2018-06-21 15:56:11 +0800

    Initial Commit

---
+++ OCClasses/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCClasses
+//
+//  Created by 李一平 on 2018/6/21.
+//  Copyright © 2018年 qingfengiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCClasses/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCClasses
+//
+//  Created by 李一平 on 2018/6/21.
+//  Copyright © 2018年 qingfengiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
